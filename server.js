124a577d49342ec90c7604b6dec52b124b428c80
Hannah Sindorf <hannah@hsindorf.com>
2018-08-29 10:36:41 -0700

    added path for show page

--- server.js
+++ server.js
@@ -12,23 +12,43 @@ const conString = process.env.DATABASE_URL;
 let client = new pg.Client(conString);
 client.connect();
 
+// home
 app.get('/', (req, res) => {
-  let SQL = 'SELECT title, author, image_url FROM books';
+  let SQL = 'SELECT title, author, image_url, id FROM books';
   client.query(SQL)
     .then(data => {
       let books = data.rows;
+      books.page = 'pages/home.ejs';
+      books.pageTitle = 'Saved Books';
       res.render('master', {items:books});
     })
     .catch(err => {
       console.log(err);
-      res.render('error');
+      res.render('pages/error');
+    });
+});
+
+// when clicking 'view details' on homepage
+app.get('/show', (req, res) => {
+  let bookId = req.body.id;
+  let SQL = `SELECT title, author, image_url, description, isbn FROM books WHERE id = ${bookId}`;
+  client.query(SQL)
+    .then(data => {
+      let books = data.rows;
+      books.page = 'pages/show.ejs';
+      books.pageTitle = 'View Details';
+      res.render('master', {items:books});
+    })
+    .catch(err => {
+      console.log(err);
+      res.render('pages/error');
     });
 });
 
 app.use(express.static('./public'));
 
 app.get('*', (req, res) => {
-  res.render('error');
+  res.render('pages/error');
 });
 
 app.listen(PORT, () => console.log('Server is up on ', PORT));
