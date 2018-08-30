31d17d0e0163c7b099bd80d59056e640009787cf
Hannah Sindorf <hannah@hsindorf.com>
2018-08-30 10:44:59 -0700

    added functioning form

--- server.js
+++ server.js
@@ -6,6 +6,7 @@ const pg = require('pg');
 
 let app = express();
 app.set('view engine', 'ejs');
+app.use(express.urlencoded({extended:true}));
 const PORT = process.env.PORT || 3000;
 
 const conString = process.env.DATABASE_URL;
@@ -14,30 +15,21 @@ client.connect();
 
 // home
 app.get('/', (req, res) => {
-  let SQL = 'SELECT title, author, image_url, id FROM books';
-  client.query(SQL)
-    .then(data => {
-      let books = data.rows;
-      res.render('master', {items:books, 'thisPage':'pages/home.ejs', 'thisPageTitle':'Saved Books'});
-    })
-    .catch(err => {
-      errorHandling(req, res, err);
-    });
+  homePage(req, res);
 });
 
 // when clicking 'view details' on homepage
 app.get('/show', (req, res) => {
-  let bookId = req.query.book;
-  let SQL = `SELECT title, author, image_url, book_description, isbn FROM books WHERE id = $1 `;
-  let values = [bookId];
-  client.query(SQL, values)
-    .then(data => {
-      let books = data.rows;
-      res.render('master', {items:books, 'thisPage':'pages/show.ejs', 'thisPageTitle':'View Details'});
-    })
-    .catch(err => {
-      errorHandling(req, res, err);
-    });
+  viewDetails(req, res);
+});
+
+// add new book
+app.get('/new', (req, res) => {
+  res.render('master', {'thisPage': 'pages/new.ejs', 'thisPageTitle': 'Add a New Book'});
+});
+
+app.post('/new/submit', (req, res) => {
+  addNew(req, res);
 });
 
 app.use(express.static('./public'));
@@ -51,4 +43,48 @@ app.listen(PORT, () => console.log('Server is up on ', PORT));
 function errorHandling(req, res, err) {
   if (err) { console.log(err); }
   res.render('master', {'thisPage':'pages/error.ejs', 'thisPageTitle':'Something broke!'});
+}
+
+function homePage(req, res) {
+  let SQL = 'SELECT title, author, image_url, id FROM books';
+  client.query(SQL)
+    .then(data => {
+      let books = data.rows;
+      res.render('master', {items:books, 'thisPage':'pages/home.ejs', 'thisPageTitle':'Saved Books'});
+    })
+    .catch(err => {
+      errorHandling(req, res, err);
+    });
+}
+
+function viewDetails(req, res) {
+  let bookId = req.query.book;
+  let SQL = `SELECT title, author, image_url, book_description, isbn FROM books WHERE id = $1 `;
+  let values = [bookId];
+  client.query(SQL, values)
+    .then(data => {
+      let books = data.rows;
+      res.render('master', {items:books, 'thisPage':'pages/show.ejs', 'thisPageTitle':'View Details'});
+    })
+    .catch(err => {
+      errorHandling(req, res, err);
+    });
+}
+
+function addNew(req, res) {
+  let SQL = `INSERT INTO books (title, author, isbn, image_url, book_description) VALUES ( $1, $2, $3, $4, $5 )`;
+  let values = [
+    req.body.title,
+    req.body.author,
+    req.body.isbn,
+    req.body.image_url,
+    req.body.book_description
+  ];
+  client.query(SQL, values)
+    .then( () => {
+      res.render('master', {'thisPage': 'pages/submit.ejs', 'thisPageTitle': 'Submitted!'});
+    })
+    .catch( () => {
+      errorHandling(req, res);
+    });
 }
